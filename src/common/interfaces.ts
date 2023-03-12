ed62b0b865249d0d8afce6a9714ab8be96f8ac35
ronenk1 <ronenk1@rnd-hub.com>
2023-03-12 11:32:33 +0200

    fix: remove unused interface

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,5 +1,4 @@
 import { IJobResponse } from '@map-colonies/mc-priority-queue';
-import { OperationStatus } from './enums';
 
 export interface IConfig {
   get: <T>(setting: string) => T;
@@ -13,16 +12,6 @@ export interface ICleanupData {
 
 export interface IExporterCleanupParameters {
   cleanupData?: ICleanupData;
-}
-
-export interface ITaskResponse<T> {
-  description: string;
-  parameters: T;
-  reason: string;
-  percentage: number;
-  type: string;
-  status: OperationStatus;
-  attempts: number;
 }
 
 export type IExporterJobResponse = IJobResponse<IExporterCleanupParameters, Record<string, unknown>>;
