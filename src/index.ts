3a0b85893d84c7847bfc8db3fefaaa7615dfa7ac
almog8k <60139576+almog8k@users.noreply.github.com>
2023-05-01 10:02:19 +0300

    chore: updating packages to latest according to ts-server-boilerplate (#16)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 /* eslint-disable import/first */
 // this import must be called before the first import of tsyring
 import 'reflect-metadata';
-import { Metrics } from '@map-colonies/telemetry';
-import { container } from 'tsyringe';
 import { tracing } from './common/tracing';
-import { SERVICES } from './common/constants';
 import { getApp } from './app';
 
 async function main(): Promise<void> {
@@ -12,9 +9,8 @@ async function main(): Promise<void> {
 
   await app.run(process.argv);
 
-  //stop tracing and metrics when app finish running
-  const metrics = container.resolve<Metrics>(SERVICES.METRICS);
-  await Promise.all([metrics.stop(), tracing.stop()]);
+  //stop tracing when app finish running
+  await Promise.all([tracing.stop()]);
 }
 
 void main();
