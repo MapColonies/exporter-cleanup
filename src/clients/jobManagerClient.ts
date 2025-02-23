b96e6caf367330b263594317f66c1fc245a75142
almog8k <almog8k@gmail.com>
2025-02-23 13:33:38 +0200

    Revert "Revert "Revert "fix: jobs missing params(MAPCO-6822)"""
    This reverts commit 46b3c922f537da8a50b99118966b4a65742e8f0f.

--- src/clients/jobManagerClient.ts
+++ src/clients/jobManagerClient.ts
@@ -2,7 +2,7 @@ import { inject, injectable } from 'tsyringe';
 import config from 'config';
 import { HttpClient, IHttpRetryConfig } from '@map-colonies/mc-utils';
 import { Logger } from '@map-colonies/js-logger';
-import { IExporterJobResponse, IJobResponseWithoutParams } from '../common/interfaces';
+import { IExporterJobResponse } from '../common/interfaces';
 import { SERVICES } from '../common/constants';
 import { OperationStatus } from '../common/enums';
 
@@ -24,12 +24,11 @@ export class JobManagerClient extends HttpClient {
       type: this.tilesJobType,
       shouldReturnTasks: false,
     };
-    const res = await this.get<IJobResponseWithoutParams[] | undefined>('/jobs', query);
+    const res = await this.get<IExporterJobResponse[] | undefined>('/jobs', query);
     if (!res) {
       return [];
     }
-    const jobsWithParams = await this.getJobsWithParams(res);
-    return jobsWithParams;
+    return res;
   }
 
   public async getFailedUncleanedJobs(): Promise<IExporterJobResponse[]> {
@@ -39,12 +38,11 @@ export class JobManagerClient extends HttpClient {
       type: this.tilesJobType,
       shouldReturnTasks: false,
     };
-    const res = await this.get<IJobResponseWithoutParams[] | undefined>('/jobs', query);
+    const res = await this.get<IExporterJobResponse[] | undefined>('/jobs', query);
     if (!res) {
       return [];
     }
-    const jobsWithParams = await this.getJobsWithParams(res);
-    return jobsWithParams;
+    return res;
   }
 
   public async updateCleaned(jobId: string): Promise<void> {
@@ -52,17 +50,5 @@ export class JobManagerClient extends HttpClient {
       isCleaned: true,
     };
     await this.put(`/jobs/${jobId}`, body);
-  }
-
-  private async getJobsWithParams(jobs: IJobResponseWithoutParams[]): Promise<IExporterJobResponse[]> {
-    const jobsWithParams = await Promise.all(jobs.map(async (job) => this.getExportJobById(job.id)));
-    this.logger.info({ msg: `Got ${jobsWithParams.length} jobs with params` });
-    return jobsWithParams;
-  }
-
-  private async getExportJobById(jobId: string): Promise<IExporterJobResponse> {
-    this.logger.debug({ msg: `Getting export job by id`, jobId });
-    const job = await this.get<IExporterJobResponse>(`/jobs/${jobId}`);
-    return job;
   }
 }
