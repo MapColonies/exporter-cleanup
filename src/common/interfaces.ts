9bfd56a052d47478811b61de034be57e69480f96
Ronit Kissis <92728393+RonitKissis@users.noreply.github.com>
2025-03-06 10:00:34 +0200

    feat!: add raster-shared support with new parameters structure(MAPCO-6887) (#24)
    * feat: add raster-shared support wih new parameters structure
    
    * fix: lint and package
    
    * Update src/clients/jobManagerClient.ts
    
    Co-authored-by: Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
    
    * Update src/clients/jobManagerClient.ts
    
    Co-authored-by: Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
    
    * fix: pr changes
    
    ---------
    
    Co-authored-by: Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,19 +1,11 @@
 import { IJobResponse } from '@map-colonies/mc-priority-queue';
+import { ExportJobParameters } from '@map-colonies/raster-shared';
 
 export interface IConfig {
   get: <T>(setting: string) => T;
   has: (setting: string) => boolean;
 }
 
-export interface ICleanupData {
-  directoryPath?: string;
-  cleanupExpirationTimeUTC?: Date;
-}
-
-export interface IExporterCleanupParameters {
-  cleanupData?: ICleanupData;
-}
-
-export type IExporterJobResponse = IJobResponse<IExporterCleanupParameters, Record<string, unknown>>;
+export type IExporterJobResponse = IJobResponse<ExportJobParameters, Record<string, unknown>>;
 
 export type IJobResponseWithoutParams = Omit<IExporterJobResponse, 'parameters'>;
